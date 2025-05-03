9489075c7bef10a92a0d6b04f98f77b90bf759fa
duranbajrami <126950376+duranbajrami@users.noreply.github.com>
2025-05-03 18:38:54 +0200

    Campus
    implemented street view

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ import {
 import Home from './Pages/Home';
 import Quiz from "./Pages/Quiz";
 import University from './Pages/University';
-
+import Campuses from './Pages/Campuses';
 
 function App() {
     return (
@@ -16,6 +16,7 @@ function App() {
                 <Route path="/" element={<Home />} />
                 <Route path={"/quiz"} element={<Quiz />} />
                 <Route path={"/universities"} element={<University />} />
+                <Route path={"/campus"} element={<Campuses />} />
             </Routes>
         </Router>
     );
